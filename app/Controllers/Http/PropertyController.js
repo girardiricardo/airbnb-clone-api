cf8f1858d6556ee6c2727728f7b137d0f83b8a2d
Ricardo Girardi <girardiricardo@outlook.com>
2018-08-07 02:17:47 -0300

    Configuring CRUD methods

--- app/Controllers/Http/PropertyController.js
+++ app/Controllers/Http/PropertyController.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Property = use('App/Models/Property')
+
 /**
  * Resourceful controller for interacting with properties
  */
@@ -9,6 +11,9 @@ class PropertyController {
    * GET properties
    */
   async index ({ request, response, view }) {
+    const properties = Property.all()
+
+    return properties
   }
 
   /**
@@ -23,6 +28,11 @@ class PropertyController {
    * GET properties/:id
    */
   async show ({ params, request, response, view }) {
+    const property = await Property.findOrFail(params.id)
+
+    await property.load('images')
+
+    return property
   }
 
   /**
@@ -37,6 +47,13 @@ class PropertyController {
    * DELETE properties/:id
    */
   async destroy ({ params, request, response }) {
+    const property = await Property.findOrFail(params.id)
+
+    if(property.user_id !== auth.user.id) {
+      return response.status(401).send({ error: 'Not authorized' })
+    }
+
+    await property.delete()
   }
 }
 
