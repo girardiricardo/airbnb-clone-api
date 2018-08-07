ba9e249f90e3eaa39433ad51553683c9ae3ae828
Ricardo Girardi <girardiricardo@outlook.com>
2018-08-07 19:37:36 -0300

    Searching properties with lat & long

--- app/Controllers/Http/PropertyController.js
+++ app/Controllers/Http/PropertyController.js
@@ -11,7 +11,11 @@ class PropertyController {
    * GET properties
    */
   async index ({ request, response, view }) {
-    const properties = Property.all()
+    const { latitude, longitude } = request.all()
+    
+    const properties = Property.query()
+      .nearBy(latitude, longitude, 10)
+      .fecth()
 
     return properties
   }
--- app/Models/Property.js
+++ app/Models/Property.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Model = use('Model')
+const Database = use ('Database')
 
 class Property extends Model {
     user () {
@@ -9,6 +10,19 @@ class Property extends Model {
 
     images () {
         return this.hasMany('App/Models/Image')
+    }
+
+    static scopeNearBy (query, latitude, longitude, distance) {
+        const haversine = `(6371 * acos(cos(radians(${latitude}))
+            * cos(radians(latitude))
+            * cos(radians(longitude)
+            - radians(${longitude}))
+            + sin(radians(${latitude}))
+            * sin(radians(latitude))))`
+        
+        return query
+            .select('*', Database.raw(`${haversine} as distance`))
+            .whereRaw(`${haversine} < ${distance}`)
     }
 }
 
