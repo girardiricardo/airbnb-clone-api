a152c28cb16e8695f5df32f85b25ac5f09f251d8
Ricardo Girardi <girardiricardo@outlook.com>
2018-08-07 01:51:21 -0300

    Adding entity relation between the models

--- app/Models/User.js
+++ app/Models/User.js
@@ -31,6 +31,10 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  properties () {
+    return this.hasMany('App/Models/Property')
+  }
 }
 
 module.exports = User
