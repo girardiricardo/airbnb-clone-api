a364bf18a1d01354601fccb3966b51c7f3b2f12f
Ricardo Girardi <girardiricardo@outlook.com>
2018-08-16 19:56:23 -0300

    Trying connect PG

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const Env = use('Env')
 const Helpers = use('Helpers')
 
 const Url = require('url-parse')
-const POSTG_DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
 module.exports = {
   /*
@@ -31,11 +31,11 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', 'POSTG_DATABASE_URL.host'),
+      host: Env.get('DB_HOST', 'DATABASE_URL.host'),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'POSTG_DATABASE_URL.username'),
-      password: Env.get('DB_PASSWORD', 'POSTG_DATABASE_URL.password'),
-      database: Env.get('DB_DATABASE', 'POSTG_DATABASE_URL.pathname.substr(1)')
+      user: Env.get('DB_USER', 'DATABASE_URL.username'),
+      password: Env.get('DB_PASSWORD', 'DATABASE_URL.password'),
+      database: Env.get('DB_DATABASE', 'DATABASE_URL.pathname.substr(1)')
     },
     debug: Env.get('DB_DEBUG', false)
   }
